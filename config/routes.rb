f7869a6b14e0a716ef75549c29ce0c2fb567e44d
Dejwoon <d.poddolny@gmail.com>
2018-01-28 16:11:35 +0100

    edit main_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  root 'welcome#index'
-
+  root 'main_page#index'
+  get 'main_page/index'
   resources :main_page
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
